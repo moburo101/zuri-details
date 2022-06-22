9d0b91671e03ff9e229ca84a38b4f90d463be41f
moburo101 <moburoarah@gmail.com>
2022-06-22 02:25:18 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log("****************************************************************");
+console.log("M'obunoburo Arah");
+console.log("Six foot two");
+console.log("Nigeria");
+console.log("****************************************************************");
